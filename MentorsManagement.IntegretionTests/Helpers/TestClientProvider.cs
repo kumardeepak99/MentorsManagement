3854274832deac5c0583cadbc6b25e8f89d677b9
kumardeepak99 <dkumarjha999@gmail.com>
2023-06-17 00:44:38 +0530

    Integration completed for fetching all mentors

---
+++ MentorsManagement.IntegretionTests/Helpers/TestClientProvider.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+
+namespace MentorsManagement.IntegrationTests.Helpers
+{
+    public class TestClientProvider
+    {
+        public HttpClient _client { get; private set; }
+
+        public TestClientProvider()
+        {
+            var server = new TestServer(new WebHostBuilder().UseStartup<TestStartup>());
+            _client = server.CreateClient();
+            _client.BaseAddress = new Uri("https://localhost:44355");
+        }
+
+        public static StringContent GetJsonHttpContent(object items)
+        {
+            return new StringContent(JsonConvert.SerializeObject(items), Encoding.UTF8, "application/json");
+        }
+
+        internal static class Urls
+        {
+            //public readonly static string BaseUrlAddress = "https://localhost:44355";
+            public readonly static string GetAllMentors = "/Mentors/GetAllAsync";
+
+        }
+
+    }
+}
---
+++ MentorsManagement.IntegretionTests/Helpers/TestStartup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MentorsManagement.IntegrationTests.Helpers
+{
+    public class TestStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            // Add any other required services for your API
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                app.UseHttpsRedirection();
+            });
+        }
+
+    }
+}
